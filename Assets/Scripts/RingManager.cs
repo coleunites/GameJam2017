b001b28d5c432565286b27f0159e93caad0fab3b
Adam Wiwcharuk <adamwiwc@stu.aii.edu>
2017-01-20 21:14:19 -0800

    Beginning of ring manager :turtle:
    added the start of the ring manager. Rings can be added to the queue.
    all items in the queue have their size decreased.

---
+++ Assets/Scripts/RingManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RingManager : MonoBehaviour {
+
+    //RingManager holds and controls the queue for each individual ring entering the game
+    private Queue<RingController> ringQueue;
+    public float scaleAboveLastMultiplier;
+    public float speedMultiplier;
+    public float startingRingSpeed;
+    public float destroyRingSize;
+
+    private float currentScaleSpeed;
+    private float scaleOfLast = 1.0f;
+
+	void Start ()
+    {
+        currentScaleSpeed = startingRingSpeed;
+	}
+	
+	void Update ()
+    {
+        currentScaleSpeed *= speedMultiplier;
+        foreach (RingController ring in ringQueue)
+        {
+            ring.Scale(currentScaleSpeed);
+            if (ring.gameObject.transform.localScale.x <= destroyRingSize)
+            {
+                //tell hermit controller to check if we survive this ring
+                //if so delete this ring, if not game over.
+            }
+        }
+	}
+
+    public void AddRingToQueue(RingController newRing )
+    {
+        //set it's size relitive to the last
+        float newScale = scaleOfLast * scaleAboveLastMultiplier;
+        scaleOfLast = newScale;
+        newRing.gameObject.transform.localScale = new Vector3(newScale, 1.0f, newScale);  
+        ringQueue.Enqueue(newRing);
+    }
+
+    
+
+}
