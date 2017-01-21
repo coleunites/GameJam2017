27edf28d1ca393aeb7cc564afa6e884f7c3c859e
Cole Rudniski <coleunites@shaw.ca>
2017-01-20 23:57:09 -0800

    finshed levelMangaer implemeation

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,15 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
+    public RingManager mRingManager;
+    public List<Section> mSections;
+    public List<Pattern> mPatterns;
+
+    public int mMinNumRings = 12;
+
+    public float mSectionCoolDown = 2.0f;
+
+    public int mCurSection = 0;
+    public int mPatternsLeftInSection = 0;
+
+    public float mCoolDownTimeRemaining = 0.0f;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        mPatternsLeftInSection = mSections[mCurSection].mNumOfPatterns;
+        AddPattern();      
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        int ringNum = mRingManager.GetQueueCount();
+
+        if(ringNum == 0 && mPatternsLeftInSection == 0)
+        {
+            mCurSection = (++mCurSection) % mSections.Count;
+            mCoolDownTimeRemaining = mSectionCoolDown;
+        }
+        else if (ringNum < mMinNumRings && mCoolDownTimeRemaining < 0.0f)
+        {
+            AddPattern();
+        }
+        else
+        {
+            //Place Color Lerp Here
+        }
+        mCoolDownTimeRemaining -= Time.deltaTime;
 	}
+
+    void AddPattern()
+    {
+        int patternNum = Random.Range(mSections[mCurSection].mStartOfSection, mSections[mCurSection].mEndOfSection + 1);
+        Pattern curPattern = mPatterns[patternNum];
+        for (int i = 0; i < curPattern.mRings.Count; ++i)
+        {
+            GameObject newRing = Instantiate(curPattern.mRings[i]);
+            RingController newRingController = newRing.GetComponent<RingController>();
+            newRingController.SetSpriteColor(mSections[mCurSection].mSectionColor);
+            mRingManager.AddRingToQueue(newRingController);
+        }
+    }
+}
+
+[System.Serializable]
+public class Section
+{
+    public int mStartOfSection = 0;
+    public int mEndOfSection = 0;
+    public int mNumOfPatterns = 0;
+    public Color mSectionColor;
+}
+
+[System.Serializable]
+public class Pattern
+{
+    public List<GameObject> mRings;
 }
